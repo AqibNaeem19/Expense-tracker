2a40a85109d4dcb1292e966ec3d1c5afb905f12b
Aqib Naeem <maqibnaeem2019@gmail.com>
2021-08-18 16:45:59 +0500

    Add styling to Date field

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import './ExpenseItem.css'
 export default function ExpenseItem(props) {
   return (
     <div className='expense-item'>
-      <div> {props.purchasedDate} </div>
+      <div className="expense-item__date"> {props.purchasedDate} </div>
       <div className='expense-item__description'>
         <h2> {props.name} </h2>
       </div>
