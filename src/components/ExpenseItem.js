26827580b6fb1bdeecacf3073c3531041d1d0556
Aqib Naeem <maqibnaeem2019@gmail.com>
2021-08-18 17:58:08 +0500

    Correct styling for responsiveness

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,13 +4,13 @@ import './ExpenseItem.css'
 export default function ExpenseItem(props) {
   return (
     <div>
-    <div className='expense-item'>
-      <ExpenseDate date={props.purchasedDate} />
-      <div className='expense-item__description'>
-        <h2> {props.name} </h2>
+      <div className='expense-item'>
+        <ExpenseDate date={props.purchasedDate} />
+        <div className='expense-item__description'>
+          <h2> {props.name} </h2>
+          <div className='expense-item__price'>$ {props.price}</div>
+        </div>
       </div>
-      <div className='expense-item__price'>$ {props.price}</div>
-    </div>
     </div>
   )
 }
