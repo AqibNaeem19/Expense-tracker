afbbf809e5560ed3a5ba263be04cfe2ef0910a39
Aqib Naeem <maqibnaeem2019@gmail.com>
2021-08-21 19:52:58 +0500

    List update and render

--- src/App.js
+++ src/App.js
@@ -1,53 +1,63 @@
+import React, { useState } from 'react';
 import Expense from './components/Expenses/Expense';
 import NewExpense from './components/NewExpense/NewExpense';
 import {generateRandomId} from './Helper/GenerateRandomId';
 
-function App() {
-  // array containing objects for various expense items.
-  const ExpenseItems = [
-    { 
-      _id: generateRandomId(), 
-      expenseItemPrice: 67.8, 
-      expenseItemName: 'Toilet Paper', 
-      itemPurchaseDate: new Date() 
-    },
-    { 
-      _id: generateRandomId(), 
-      expenseItemPrice: 15.77, 
-      expenseItemName: 'Skimmed Milk', 
-      itemPurchaseDate: new Date() 
-    },
-    { 
-      _id: generateRandomId(), 
-      expenseItemPrice: 203.99, 
-      expenseItemName: 'Groceries', 
-      itemPurchaseDate: new Date()
-    },
-    { 
-      _id: generateRandomId(), 
-      expenseItemPrice: 500, 
-      expenseItemName: 'Health Insuranse', 
-      itemPurchaseDate: new Date()
-    },
-    { 
-      _id: generateRandomId(), 
-      expenseItemPrice: 55.06, 
-      expenseItemName: 'Jogging Shoes', 
-      itemPurchaseDate: new Date()
-    }
-  ]
 
+const initialExpenseData = [
+  { 
+    _id: generateRandomId(), 
+    expenseItemPrice: 67.8, 
+    expenseItemName: 'Toilet Paper', 
+    itemPurchaseDate: new Date(2020, 6, 23)
+  },
+  { 
+    _id: generateRandomId(), 
+    expenseItemPrice: 15.77, 
+    expenseItemName: 'Skimmed Milk', 
+    itemPurchaseDate: new Date(2021, 3, 12)
+  },
+  { 
+    _id: generateRandomId(), 
+    expenseItemPrice: 203.99, 
+    expenseItemName: 'Groceries', 
+    itemPurchaseDate: new Date(2021, 5, 16)
+  },
+  { 
+    _id: generateRandomId(), 
+    expenseItemPrice: 500, 
+    expenseItemName: 'Health Insuranse', 
+    itemPurchaseDate: new Date(2021, 5, 4)
+  },
+  { 
+    _id: generateRandomId(), 
+    expenseItemPrice: 55.06, 
+    expenseItemName: 'Jogging Shoes', 
+    itemPurchaseDate: new Date(2021, 10, 12)
+  }
+]
+
+
+function App() {
+  const [expenses, setExpenses] = useState(initialExpenseData);
+
+  // Update List function.
   const addNewExpenseHandler = newExpensedData => {
+
     const newExpense = {...newExpensedData,
       _id: generateRandomId()
     };
-    console.log(newExpense);
+
+    setExpenses((prevState) => {
+      return [newExpense, ...prevState];
+    })
+    
   }
 
   return (
     <div className="App">
       <NewExpense addNewExpense={addNewExpenseHandler} />
-      <Expense expenseList={ExpenseItems}/>
+      <Expense expenseList={expenses} />
     </div>
   );
 }
